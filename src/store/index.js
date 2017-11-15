f92f056834ca41496a76f4f9a42e9614d583834f
jicjjang <jicjjang12@gmail.com>
2017-11-16 01:29:52 +0900

    append vuex set

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import auth from './modules/auth'
+import feed from './modules/feed'
+import pub from './modules/pub'
+import beer from './modules/beer'
+import news from './modules/news'
+
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+export default new Vuex.Store({
+  modules: {
+    auth,
+    feed,
+    pub,
+    beer,
+    news
+  }
+})
