cbed330756ed0a63ff28fa925076691a9316c2da
jicjjang <jicjjang12@gmail.com>
2017-11-16 01:30:23 +0900

    append router set

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import Feed from '@/container/Feed'
+import Pub from '@/container/Pub'
+import PubDetail from '@/container/PubDetail'
+import PubRank from '@/container/PubRank'
+import Beer from '@/container/Beer'
+import BeerDetail from '@/container/BeerDetail'
+import BeerRank from '@/container/BeerRank'
+import News from '@/container/News'
 
 Vue.use(Router)
 
@@ -8,8 +16,58 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Feed',
+      component: Feed
+    },
+    {
+      path: '/pub',
+      name: 'Pub',
+      component: Pub
+    },
+    {
+      path: '/pub/:id',
+      name: 'PubDetail',
+      component: PubDetail
+    },
+    {
+      path: '/pub/rank',
+      name: 'PubRank',
+      component: PubRank
+    },
+    {
+      path: '/beer',
+      name: 'Beer',
+      component: Beer
+    },
+    {
+      path: '/beer/:id',
+      name: 'BeerDetail',
+      component: BeerDetail
+    },
+    {
+      path: '/',
+      name: 'BeerRank',
+      component: BeerRank
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: News
     }
-  ]
+  ],
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 })
