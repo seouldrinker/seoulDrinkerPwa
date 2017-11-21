e2858bbb546d184f4a10d0ab4278a1da76194def
jicjjang <jicjjang12@gmail.com>
2017-11-21 22:59:52 +0900

    append common list for beer and pub

---
+++ src/components/beerPub/list/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="beerPubHeader">
+    <span class="image">
+      <img :src="image" alt="SeoulDrinker">
+    </span>
+    <span class="beerPubInfo">
+      <div class="engName">
+        {{ data.eng_name }}
+      </div>
+      <div class="korName">
+        {{ data.kor_name }}
+      </div>
+      <app-beer-item-contents v-if="isBeer"
+        :data="data"
+      />
+      <app-pub-item-contents v-else
+        :data="data"
+      />
+    </span>
+  </div>
+</template>
+
+<script>
+import PubItemContents from '../../pub/list/contents'
+import BeerItemContents from '../../beer/list/contents'
+
+export default {
+  components: {
+    appPubItemContents: PubItemContents,
+    appBeerItemContents: BeerItemContents
+  },
+  props: ['isBeer', 'data', 'image', 'contents'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .beerPubHeader {
+
+  }
+</style>
