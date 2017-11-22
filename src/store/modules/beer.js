3937f25d847192b1dfe84b923733841cfaa2fd46
jicjjang <jicjjang12@gmail.com>
2017-11-22 18:25:25 +0900

    append store to retrieve pub and beer detail.

--- src/store/modules/beer.js
+++ src/store/modules/beer.js
@@ -4,18 +4,25 @@ import axios from 'axios'
 import { API_URL } from '../../config'
 
 const state = {
-  beerList: null
+  beerList: null,
+  beerDetail: null
 }
 
 const getters = {
   getBeerList: () => {
     return state.beerList
+  },
+  getBeerDetail: () => {
+    return state.beerDetail
   }
 }
 
 const mutations = {
   'RETRIEVE_BEER_LIST' (state, results) {
     Vue.set(state, 'beerList', results)
+  },
+  'RETRIEVE_BEER_DETAIL' (state, results) {
+    Vue.set(state, 'beerDetail', results)
   }
 }
 
@@ -24,6 +31,17 @@ const actions = {
     axios.get(`${API_URL}/beer`).then(results => {
       if (results && results.data && results.data.results) {
         commit('RETRIEVE_BEER_LIST', results.data.results)
+        return true
+      }
+      throw new Error('Retrieve Error!')
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  retrieveBeerDetail: ({ commit }, _id) => {
+    axios.get(`${API_URL}/beer/${_id}`).then(results => {
+      if (results && results.data && results.data.results) {
+        commit('RETRIEVE_BEER_DETAIL', results.data.results)
         return true
       }
       throw new Error('Retrieve Error!')
--- src/store/modules/pub.js
+++ src/store/modules/pub.js
@@ -4,18 +4,25 @@ import axios from 'axios'
 import { API_URL } from '../../config'
 
 const state = {
-  pubList: null
+  pubList: null,
+  pubDetail: null
 }
 
 const getters = {
   getPubList: () => {
     return state.pubList
+  },
+  getPubDetail: () => {
+    return state.pubDetail
   }
 }
 
 const mutations = {
   'RETRIEVE_PUB_LIST' (state, results) {
     Vue.set(state, 'pubList', results)
+  },
+  'RETRIEVE_PUB_DETAIL' (state, results) {
+    Vue.set(state, 'pubDetail', results)
   }
 }
 
@@ -24,6 +31,17 @@ const actions = {
     axios.get(`${API_URL}/pub`).then(results => {
       if (results && results.data && results.data.results) {
         commit('RETRIEVE_PUB_LIST', results.data.results)
+        return true
+      }
+      throw new Error('Retrieve Error!')
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  retrievePubDetail: ({ commit }, _id) => {
+    axios.get(`${API_URL}/pub/${_id}`).then(results => {
+      if (results && results.data && results.data.results) {
+        commit('RETRIEVE_PUB_DETAIL', results.data.results)
         return true
       }
       throw new Error('Retrieve Error!')
